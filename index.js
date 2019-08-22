288097e42fbcd4a41c84f66f37132f29ff0f748f
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2019-08-22 13:52:20 +0100

    from codepen

---
+++ index.js
@@ -0,0 +1,103 @@
+var slides = document.getElementsByClassName('pose');
+var dots = document.getElementsByClassName('dot'); 
+var playPauseButton = document.getElementById('playPause');
+
+function start() {
+slides[0].style.display = 'block';
+dots[0].className += ' active';
+playPauseButton.innerHTML = '&#9658;';
+playPauseButton.classList.add('start');
+}
+
+start();
+
+var slideIndex = 1;
+var slideInterval = setInterval(slideShow, 2000);
+var playing = false;
+
+function slideShow() {
+  var i;
+  
+  if (playing) {
+  playPauseButton.className = playPauseButton.className.replace('start', '');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  
+  slideIndex++;
+  
+  if (slideIndex > (slides.length += 1)) {slideIndex = 2}
+  
+  slides[slideIndex-2].style.display = 'block';
+  dots[slideIndex-2].className += ' active';
+  slideInterval = (slideShow, 2000);
+  }
+ }
+
+slideShow();
+
+function pauseSlideShow() {
+  playing = false;
+  clearInterval(slideInterval);
+  playPauseButton.innerHTML = '&#9658;';
+  }
+
+function playSlideShow() {
+  playing = true;
+  slideInterval = (slideShow, 2000);
+  playPauseButton.innerHTML = '&#9616;&nbsp;&#9612';
+  }
+
+function togglePlay() {
+  if (playing) {
+    pauseSlideShow();
+  }
+  else {
+    playSlideShow();
+  }
+}
+
+function showSlides(n) {
+  playing = false;
+  clearInterval(slideInterval);
+  var i;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += ' active';
+  playPauseButton.innerHTML = '&#9658;';
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function leftArrowPressed() {
+  showSlides(slideIndex -= 1);
+}
+
+function rightArrowPressed() {
+  showSlides(slideIndex += 1);
+}
+
+document.onkeydown = function(event) {
+  switch (event.keyCode) {
+    case 37: leftArrowPressed();
+      break;
+    case 39: rightArrowPressed();
+      break;
+  };
+}
